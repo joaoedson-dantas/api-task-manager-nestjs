078652f9dcafa77a25b0867895a933e1e25c8616
João Edson <joaoedson.dantas@outlook.com>
2023-10-24 18:56:55 -0300

    feat: implementing hash encryption on user password

--- src/models/users/useCases/create-user.usecase.ts
+++ src/models/users/useCases/create-user.usecase.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/infra/database/prisma.service';
 import { CreateUserDTO } from '../dto/user.dto';
+import { hash } from 'bcrypt';
 
-@Injectable() // Informar para o nest gerenciar essa classe
+@Injectable() // Informar para o nest gerenciar essa classe - Algo vai chamar isso
 export class CreateUserUseCase {
   constructor(private prisma: PrismaService) {}
 
@@ -14,11 +15,16 @@ export class CreateUserUseCase {
     });
 
     if (user) {
-      throw new Error('User Already exists!');
+      throw new HttpException('User Already exists!', HttpStatus.BAD_REQUEST);
     }
 
+    const password_hash = await hash(data.password, 10);
+
     return await this.prisma.user.create({
-      data,
+      data: {
+        ...data,
+        password: password_hash,
+      },
     });
   }
 }
